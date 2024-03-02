ddfcdb5bcee38611a011ceea3933d6d6656eaba0
Ouroboros Coding Inc <chris@ouroboroscoding.com>
2024-03-02 12:10:29 -0500

    Hotfix 1.0.8 - Updated version - Updated brain-oc to 1.0.9 - Updated rest-oc to 1.3.0 - Added config-oc to project and replaced Conf calls - Added tools-oc to project and replaced DictHelper calls

--- mouth/__main__.py
+++ mouth/__main__.py
@@ -10,14 +10,15 @@ __copyright__	= "Ouroboros Coding Inc."
 __email__		= "chris@ouroboroscoding.com"
 __created__		= "2022-12-12"
 
+# Ouroboros imports
+from config import config
+
 # Python imports
-import os
-import platform
 import sys
 
 # Pip imports
 from body import errors
-from RestOC import Conf, EMail, Record_MySQL, REST, Services, Session
+from RestOC import EMail, Record_MySQL, REST, Services, Session
 
 # Module imports
 from . import Mouth
@@ -32,22 +33,17 @@ def cli():
 		uint
 	"""
 
-	# Load the config
-	Conf.load('config.json')
-	sConfOverride = 'config.%s.json' % platform.node()
-	if os.path.isfile(sConfOverride):
-		Conf.load_merge(sConfOverride)
-
-	# Get Brain config
-	dConfig = Conf.get('mouth', {
-		'mysql_host': 'mouth'
+	# Get Mouth config
+	dConfig = config.mouth({
+		'mysql_host': 'mouth',
+		'verbose': False
 	})
 
 	# Add the global prepend
-	Record_MySQL.db_prepend(Conf.get(('mysql', 'prepend'), ''))
+	Record_MySQL.db_prepend(config.mysql.prepend(''))
 
 	# Add the primary mysql DB
-	Record_MySQL.add_host('mouth', Conf.get(('mysql', 'hosts', dConfig['mysql_host']), {
+	Record_MySQL.add_host('mouth', config.mysql.hosts[dConfig['mysql_host']]({
 		'host': 'localhost',
 		'port': 3306,
 		'charset': 'utf8',
@@ -57,7 +53,7 @@ def cli():
 
 	# Set the timestamp timezone
 	Record_MySQL.timestamp_timezone(
-		Conf.get(('mysql', 'timestamp_timezone'), '+00:00')
+		config.mysql.timestamp_timezone('+00:00')
 	)
 
 	# If we are installing
@@ -65,7 +61,7 @@ def cli():
 		return install()
 
 	# Init the email module
-	EMail.init(Conf.get('email', {
+	EMail.init(config.email({
 		'error_to': 'errors@localhost',
 		'from': 'admin@localhost',
 		'smtp': {
@@ -77,19 +73,11 @@ def cli():
 		}
 	}))
 
-	# Get redis session config
-	dRedis = Conf.get(('redis', 'session'), {
-		'host': 'localhost',
-		'port': 6379,
-		'db': 0,
-		'charset': 'utf8'
-	})
-
 	# Init the Session module
-	Session.init(dRedis)
+	Session.init('session')
 
 	# Get the REST config
-	dRest = Conf.get('rest', {
+	dRest = config.rest({
 		'allowed': 'localhost',
 		'default': {
 			'domain': 'localhost',
@@ -108,7 +96,7 @@ def cli():
 	oRestConf = REST.Config(dRest)
 
 	# Set verbose mode if requested
-	if 'VERBOSE' in os.environ and os.environ['VERBOSE'] == '1':
+	if dConfig['verbose']:
 		Services.verbose()
 
 	# Get all the services
@@ -121,8 +109,8 @@ def cli():
 	Services.register(
 		dServices,
 		oRestConf,
-		Conf.get(('services', 'salt')),
-		Conf.get(('services', 'internal_key_timeout'), 10)
+		config.services.salt(),
+		config.services.internal_key_timeout(10)
 	)
 
 	# Create the HTTP server and map requests to service
@@ -146,13 +134,15 @@ def cli():
 
 		},
 		'mouth',
-		'https?://(.*\\.)?%s' % Conf.get(('rest', 'allowed')).replace('.', '\\.'),
-		error_callback=errors.service_error
+		'https?://(.*\\.)?%s' % config.rest.allowed('localhost').replace('.', '\\.'),
+		error_callback = errors.service_error
 	).run(
-		host=oRestConf['mouth']['host'],
-		port=oRestConf['mouth']['port'],
-		workers=oRestConf['mouth']['workers'],
-		timeout='timeout' in oRestConf['mouth'] and oRestConf['mouth']['timeout'] or 30
+		host = oRestConf['mouth']['host'],
+		port = oRestConf['mouth']['port'],
+		workers = oRestConf['mouth']['workers'],
+		timeout = 'timeout' in oRestConf['mouth'] and \
+					oRestConf['mouth']['timeout'] or \
+					30
 	)
 
 	# Return OK
